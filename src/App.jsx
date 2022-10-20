f58a28d5b8651b638bf303a385d5fb8688481a35
wbsantiago <will.jimmy27@hotmail.com>
2022-10-20 17:50:44 -0300

    done layout and styled of first screen, BeginQuiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import BeginQuiz from './components/BeginQuiz'
 
 function App() {
 
   return (
     <div className="App">
+      <BeginQuiz />
     </div>
   )
 }
---
+++ src/components/BeginQuiz.jsx
@@ -0,0 +1,11 @@
+import './beginQuiz.css'
+
+export default function BeginQuiz() {
+    return (
+        <div className="starter--div">
+            <h1 className='starter--title'>Quizzical</h1>
+            <p className='starter--description'>Some description if needed</p>
+            <button className='starter--btn'>Start quiz</button>
+        </div>
+    )
+}
